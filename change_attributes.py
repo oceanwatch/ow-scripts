5ca33acd8a94146a7f16e8f58f24bebc6caf80f5
Melanie Abecassis <1513497446@mil>
2019-02-19 16:09:33 -1000

    added code to edit time stamp of duplicated files (due to missing files from CW central)

--- change_attributes.py
+++ change_attributes.py
@@ -21,3 +21,11 @@ time_diff = end - start
 print('All files modified in ' + str(time_diff.seconds) + 's')
 
 
+
+
+#for time stamps, for the duplicate files
+ncatted -O -a start_time,global,o,c,20180317T000000Z 20180321000000-gp-8day.nc
+ncatted -O -a time_coverage_start,global,o,c,20180317T000000Z 20180321000000-gp-8day.nc
+ncatted -O -a stop_time,global,o,c,20180318T000000Z 20180321000000-gp-8day.nc
+ncatted -O -a time_coverage_end,global,o,c,20180318T000000Z 20180321000000-gp-8day.nc
+
