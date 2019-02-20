22f419123bb9973575a0c990fb603847d6b5a741
Melanie Abecassis <1513497446@mil>
2019-02-19 16:12:53 -1000

    Added error message if no new file on ftp

--- get_data.py
+++ get_data.py
@@ -2,7 +2,7 @@ from ftplib import FTP
 from datetime import datetime
 import os 
 import glob
-from subprocess import call
+from subprocess import *
 
 start = datetime.now()
 
@@ -26,29 +26,33 @@ for i in range(len(list_local)):
 # find missing files
 diff=list(set(files)-set(list_local))
 print(diff)
-print('downloading ' + str(len(diff)) + ' file(s) for ' + str(y))
 
-#if len(diff)==0:
+if len(diff)==0:
+    body="/mnt/r01/data/goes-poes_ghrsst/bin/get_data.py did not find a new daily file in ftp://ftp.star.nesdis.noaa.gov/pub/socd2/coastwatch/sst_blended/sst5km/night/ghrsst/2019/"
+    print(body)
+    readBody = Popen(["/bin/echo", body], stdout=PIPE)
+    mail = Popen(["/bin/mail", "-s", "No new file for goes-poes-1d", "melanie.abecassis@noaa.gov"], stdin=readBody.stdout, stdout=PIPE)
+    output = mail.communicate()[0]
 
-
-
-# download only missing files
-for file in diff:
-    print("Downloading..." + file)
-    ftp.retrbinary("RETR " + file ,open(file, 'wb').write)
-    new_file=os.path.splitext(file)[0]+"-0-360.nc"
-
-    #shift longitudes to 0-360º
-    call(["ncks", "-O", "--msa_usr_rdr", "-d", "lon,0.0,180.0", "-d", "lon,-180.0,0.0",file,new_file])
-    call(["ncap2", "-O", "-s", "where(lon<0) lon=lon+360", new_file, new_file])
-    #edit attributes
-    call(["ncatted","-O","-a","valid_min,lon,o,f,0.0",new_file])
-    call(["ncatted","-O","-a","valid_max,lon,o,f,360.0", new_file])
-    call(["ncatted","-O","-a","westernmost_longitude,global,o,f,0.0",new_file])
-    call(["ncatted","-O","-a","easternmost_longitude,global,o,f,360.0",new_file])
-    #move files
-    call(["mv",new_file,"/mnt/r01/data/goes-poes_ghrsst/daily/"])
-    call(["rm",file])
+if len(diff)>0:
+    # download only missing files
+    print('downloading ' + str(len(diff)) + ' file(s) for ' + str(y))
+    for file in diff:
+        print("Downloading..." + file)
+        ftp.retrbinary("RETR " + file ,open(file, 'wb').write)
+        new_file=os.path.splitext(file)[0]+"-0-360.nc"
+    
+        #shift longitudes to 0-360º
+        call(["ncks", "-O", "--msa_usr_rdr", "-d", "lon,0.0,180.0", "-d", "lon,-180.0,0.0",file,new_file])
+        call(["ncap2", "-O", "-s", "where(lon<0) lon=lon+360", new_file, new_file])
+        #edit attributes
+        call(["ncatted","-O","-a","valid_min,lon,o,f,0.0",new_file])
+        call(["ncatted","-O","-a","valid_max,lon,o,f,360.0", new_file])
+        call(["ncatted","-O","-a","westernmost_longitude,global,o,f,0.0",new_file])
+        call(["ncatted","-O","-a","easternmost_longitude,global,o,f,360.0",new_file])
+        #move files
+        call(["mv",new_file,"/mnt/r01/data/goes-poes_ghrsst/daily/"])
+        call(["rm",file])
 
 ftp.close()
 
