87cdc17a07d2c74a81ea2a0d38ae8a54c8ad9ea6
Melanie Abecassis <1513497446@mil>
2021-04-26 15:47:22 -0400

    2021 update

--- safe-indicators/Tom/DHW/dhw.R
+++ safe-indicators/Tom/DHW/dhw.R
@@ -1,10 +1,13 @@
-T=read.table('howland_baker.txt',header=FALSE,skip=1)
+T=read.table('2021/wake_atoll.txt',header=FALSE,skip=22)
 
 T2=T[10593:dim(T)[1],]
 library(date)
 dates=mdy.date(T2[,2],T2[,3],T2[,1])
-postscript('HB-dhw.ps')
-plot(dates,T2[,9],type='l',ylab='Degree Heating Weeks',xlab='')
+postscript('2021/Wake-dhw.ps')
+plot(dates,T2[,9],type='l',ylab='Degree Heating Weeks',xlab='',axes=FALSE)
+axis(2)
+axis(1,seq(as.numeric(dates[1]),as.numeric(dates[length(dates)]),366),2014:2021)
+box()
 dev.off()
 
 
