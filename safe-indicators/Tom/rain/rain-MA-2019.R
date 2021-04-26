cab00a39c1d93ea220c9d42f2705e1f253721f98
Melanie Abecassis <1513497446@mil>
2021-04-26 15:47:47 -0400

    2021 update

--- safe-indicators/Tom/rain/rain-MA-2019.R
+++ safe-indicators/Tom/rain/rain-MA-2019.R
@@ -1,7 +1,7 @@
 library(ncdf4)
 
 #time-series
-nc=nc_open('rain-AS-1979-2019.nc')
+nc=nc_open('2021/rain-MH-1979-2020.nc')
 v1=nc$var[[1]]
 rain=ncvar_get(nc,v1)
 dates=as.POSIXlt(v1$dim[[3]]$vals,origin='1970-01-01',tz='GMT')
@@ -11,11 +11,14 @@ for (i in 1:dim(rain)[3]) ts[i]=mean(rain[,,i],na.rm=TRUE)
 
 n=length(ts)
 
+
+x11(width=11.06,height=5.8)
+
 plot(1:(n-12),ts[1:(n-12)],type='l',axes=FALSE,xlab='',main='Precipitation',ylab='(mm/day)',pch=20,lwd=4, col=1,xlim=c(1,n))
 lines((n-12):n,ts[(n-12):n],col="#5dade2",lwd=4)
 axis(2)
 #dates[1]=1998-07-01 GMT
-axis(1,seq(1,n,12),1979:2019)
+axis(1,seq(1,n,12),1979:2020)
 box()
 
 # anomaly
@@ -31,14 +34,14 @@ mtot=rep(m,y)
 
 anom=ts-mtot
 T=data.frame(dates, ts,anom)
-write.csv(T,'rain-AS.csv',row.names=TRUE)
+write.csv(T,'2021/rain-Johnston.csv',row.names=TRUE)
 
 
 
 plot(1:(n-12),anom[1:(n-12)],type='l',axes=FALSE,xlab='',main='Precipitation',ylab='(mm/day)',pch=20,lwd=4,xlim=c(1,n))
 lines((n-12):n,anom[(n-12):n],col="#5dade2",lwd=4)
 axis(2)
-axis(1,seq(1,n,12),1979:2019)
+axis(1,seq(1,n,12),1979:2020)
 box()
 lines(c(-10,n+10),c(0,0))
 
